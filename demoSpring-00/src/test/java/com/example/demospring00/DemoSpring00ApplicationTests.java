48b9e6647090cbd8545be208e2ac913b8e5af16e
秦某某 <oncwnuCU4TDGfxCZiwYU-hwdZeoQ@git.weixin.qq.com>
2024-10-27 13:29:09 +0800

    基底文件提交

---
+++ demoSpring-00/src/test/java/com/example/demospring00/DemoSpring00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospring00;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpring00ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
